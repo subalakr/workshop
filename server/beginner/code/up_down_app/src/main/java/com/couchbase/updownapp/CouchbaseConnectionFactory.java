5cb0e233b43c33ab90e186b3c4c4ede7a88ef91c
Philipp Fehre <philipp.fehre@googlemail.com>
2014-10-30 11:16:54 +0000

    expose the sync and async bucket

--- server/beginner/code/up_down_app/src/main/java/com/couchbase/updownapp/CouchbaseConnectionFactory.java
+++ server/beginner/code/up_down_app/src/main/java/com/couchbase/updownapp/CouchbaseConnectionFactory.java
@@ -1,26 +1,25 @@
 package com.couchbase.updownapp;
 
 import com.couchbase.client.java.AsyncBucket;
+import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.CouchbaseCluster;
 
 public class CouchbaseConnectionFactory {
 
   private static AsyncBucket defaultBucket = null;
-  private static CouchbaseCluster cluster = null;
+  private static Bucket defaultBucketSync = null;
 
   public static AsyncBucket getDefaultConnection() {
     if(defaultBucket == null) {
-      // open the bucket in async mode, because we want to use Rx everywhere
-      defaultBucket = getCluster().openBucket().async();
+      defaultBucket = getDefaultConnectionSync().async();
     }
     return defaultBucket;
   }
 
-  public static CouchbaseCluster getCluster() {
-    if(cluster == null) {
-      cluster =  CouchbaseCluster.create();
+  public static Bucket getDefaultConnectionSync() {
+    if(defaultBucketSync == null) {
+      defaultBucketSync = CouchbaseCluster.create().openBucket();
     }
-    return cluster;
+    return defaultBucketSync;
   }
-
 }
