903cb1f3791a7c3c2fa9d1de501bc0653fedc20f
Michael Nitschinger <michael@nitschinger.at>
2015-05-21 19:18:20 +0200

    add RYOW

--- server/beginner/code/8_advanced_n1ql/src/main/java/com/couchbase/workshop/QueryController.java
+++ server/beginner/code/8_advanced_n1ql/src/main/java/com/couchbase/workshop/QueryController.java
@@ -1,5 +1,13 @@
 package com.couchbase.workshop;
 
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.query.Query;
+import com.couchbase.client.java.query.QueryParams;
+import com.couchbase.client.java.query.QueryResult;
+import com.couchbase.client.java.query.consistency.ScanConsistency;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -8,15 +16,30 @@ import java.util.List;
 @RequestMapping("/query")
 public class QueryController {
 
-    @RequestMapping("/readYourWrite")
-    public List<String> readYourWrite() {
+    private final Bucket bucket;
 
-        return null;
+    @Autowired
+    public QueryController(final Bucket bucket) {
+        this.bucket = bucket;
+    }
+
+    @RequestMapping("/readYourWrite")
+    public Integer readYourWrite() {
+        bucket.upsert(
+            JsonDocument.create("doc-" + System.nanoTime(), JsonObject.create().put("type", "counter"))
+        );
+
+        QueryResult query = bucket.query(Query.simple(
+            "SELECT COUNT(*) as cnt FROM default WHERE type = 'counter'",
+            QueryParams.build().consistency(ScanConsistency.REQUEST_PLUS)
+        ));
+
+        return query.allRows().get(0).value().getInt("cnt");
     }
 
     @RequestMapping("/prepared")
     public List<String> prepared() {
-        
+
         return null;
     }
 
