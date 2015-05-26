f61b264194a1f476b77cd46dd3c7c93b0bf20c19
Michael Nitschinger <michael@nitschinger.at>
2015-05-26 20:21:06 +0200

    adding initial sdk intro

---
+++ server/code/4_sdk_intro/initial/src/main/java/com/couchbase/workshop/SimpleController.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2015 Couchbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
+ * IN THE SOFTWARE.
+ */
+package com.couchbase.workshop;
+
+import com.couchbase.client.java.Bucket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class SimpleController {
+
+    private final Bucket bucket;
+
+    @Autowired
+    public SimpleController(Bucket bucket) {
+        this.bucket = bucket;
+    }
+
+    @RequestMapping("/hello")
+    public Map<String, Object> insert() {
+
+        // TODO implement me
+
+        return null;
+    }
+
+    @RequestMapping("/world")
+    public Integer read() {
+
+        // TODO implement me
+
+        return null;
+    }
+
+
+}
