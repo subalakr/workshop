385aa01ef3e84f2e0110295f7766513a55ec1f86
martin esmann <martin.esmann@hotmail.com>
2015-06-02 00:35:01 +0200

    Initial dotnet - lab 04

---
+++ server/code.net/4_sdk_intro/initial/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Couchbase.Core;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBucket bucket = null;
+
+        public HomeController()
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [ActionName("Hello")]
+        public ActionResult Insert()
+        {
+            // ToDo
+            throw new NotImplementedException();
+        }
+
+        [ActionName("World")]
+        public ActionResult Read()
+        {
+            // ToDo
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ server/code.net/4_sdk_intro/initial/WebApplication1/WebApplication1/Workshop/DatabaseConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Couchbase;
+using Couchbase.Core;
+
+namespace WebApplication1.Workshop
+{
+    public class DatabaseConfig
+    {
+        private string hostName = null;
+
+        private string bucketName = null;
+
+        private string password = null;
+
+        public Cluster Cluster()
+        {
+            // ToDo
+            throw new NotImplementedException();
+        }
+
+        public IBucket Bucket() {
+            // ToDo
+            throw new NotImplementedException();
+        }
+    }
+}
