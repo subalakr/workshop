7d21ee2431109a7b69738c43e50660b0a88ecd19
Simon Baslé <simon.basle@gmail.com>
2015-06-01 19:52:57 -0700

    use GSI in the comment about creating secondary indexes

--- server/code/6_beginner_n1ql/finished/src/main/java/com/couchbase/workshop/FlightpathController.java
+++ server/code/6_beginner_n1ql/finished/src/main/java/com/couchbase/workshop/FlightpathController.java
@@ -55,8 +55,8 @@ public class FlightpathController {
      * If you only have a primary index, this query will take 30 seconds or so! Speed it up with
      * these two indexes:
      *
-     *  - create index idx_dest on `travel-sample` (destinationairport);
-     *  - create index idx_source on `travel-sample` (sourceairport);
+     *  - create index idx_dest on `travel-sample` (destinationairport) using GSI;
+     *  - create index idx_source on `travel-sample` (sourceairport) using GSI;
      *
      * They will bring it down to a few hundred ms...
      *
